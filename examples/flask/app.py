12314a262dbe6b4406f52d02f1595173393523e4
Daniel Tahara <dktahara@gmail.com>
2019-09-18 01:37:02 +0000

    flask is example

--- examples/flask/app.py
+++ examples/flask/app.py
@@ -1,10 +1,18 @@
+import yaml
+
 from flask import Flask, jsonify, request
 import kubernetes
 
+<<<<<<< HEAD
 from k8s_jobs.manager import JobManager, JobSigner
+||||||| merged common ancestors
+from k8s_jobs.k8s.job import JobManager, JobSigner
+=======
+from k8s_jobs.k8s.config import JobDefinitionsConfig
+from k8s_jobs.k8s.job import JobSigner, JobManager
+>>>>>>> flask is example
 
 app = Flask(__name__)
-
 
 @app.route("/jobs", methods=["GET"])
 def list():
@@ -20,19 +28,28 @@ def create(job_definition_name: str):
 
     return jsonify({"job_name": job_name})
 
-
 @app.route("/healthcheck", methods=["GET"])
 def healthcheck():
     return jsonify({"healthy": True})
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     kubernetes.config.load_kube_config()
 
     app = Flask(__name__)
 
-    signer = JobSigner("signer")
-    manager = JobManager("default", signer, {})
+    namespace = app.config["JOB_NAMESPACE"]
+    signer = JobSigner(app.config["JOB_SIGNATURE"])
+
+    with open(app.config["JOB_DEFINITIONS_PATH"]) as f:
+        job_definitions = yaml.safe_load(f)
+
+    # TODO: Set default on config object
+    config_root = app.config.get("JOB_DEFINITIONS_CONFIG_ROOT", "/etc/config")
+
+    config = JobDefinitionsConfig(job_definitions, config_root)
+
+    manager = JobManager(namespace, signer, config.make_generators())
+
     retention_period_sec = int(app.config.get("JOB_RETENTION_PERIOD_SEC", "3600"))
     _ = manager.run_background_cleanup(retention_period_sec=retention_period_sec)
 
--- k8s_jobs/flask/ops.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, jsonify
-
-ops = Blueprint("ops", __name__, url_prefix="/ops")
-
-
-@ops.route("/healthcheck", methods=["GET"])
-def healthcheck():
-    return jsonify({"healthy": True})
