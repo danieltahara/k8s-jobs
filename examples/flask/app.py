1278715f6d15b8cf2a055fca3582667ca31586f8
Daniel Tahara <dktahara@gmail.com>
2019-09-22 19:16:21 +0000

    black stuff

--- examples/flask/app.py
+++ examples/flask/app.py
@@ -14,21 +14,25 @@ def create(job_definition_name: str):
 
     return jsonify({"job_name": job_name})
 
+
 @app.route("/jobs", methods=["GET"])
 def list():
     job_definition_name = request.args.get("job_definition_name", None)
     jobs = app.manager.fetch_jobs(job_definition_name)
     return jsonify({"jobs": {job.metadata.name: job.metadata.status for job in jobs}})
 
+
 @app.route("/jobs/<job_name>/status", methods=["GET"])
 def status(job_name: str):
     status = app.manager.job_status()
     return jsonify(status)
 
+
 @app.route("/jobs/<job_name>/logs", methods=["GET"])
 def logs(job_name: str):
     # NOT JSON!
     return app.manager.job_logs(job_name=job_name)
+
 
 @app.route("/healthcheck", methods=["GET"])
 def healthcheck():
@@ -45,8 +49,10 @@ if __name__ == "__main__":
     manager = config.generate()
 
     retention_period_sec = int(app.config.get("JOB_RETENTION_PERIOD_SEC", "3600"))
-    _ = manager.run_background_cleanup(retention_period_sec=retention_period_sec, delete_callback=print)
+    _ = manager.run_background_cleanup(
+        retention_period_sec=retention_period_sec, delete_callback=print
+    )
 
     app.manager = manager
 
-    app.run('0.0.0.0')
+    app.run("0.0.0.0")
